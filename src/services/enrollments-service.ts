7904f4be489ebd207037d9647a3760c91da4d591
vitoriacurty <vitoria.curty@hotmail.com>
2023-09-15 02:16:57 -0300

    fix bug cep

--- src/services/enrollments-service.ts
+++ src/services/enrollments-service.ts
@@ -11,7 +11,7 @@ async function getAddressFromCEP(cep: string) {
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
   // TODO: Tratar regras de negócio e lanças eventuais erros
-  if (!result.data) {
+  if (!result.data || result.status === httpStatus.BAD_REQUEST || result.data.erro) {
     throw notFoundError();
   }
 
